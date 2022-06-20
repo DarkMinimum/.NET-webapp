1779aacad7592870e8b033ec9e377c06b05b054e
Danil <56466408+DarkMinimum@users.noreply.github.com>
2022-06-20 21:50:24 +0300

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using PlantsShop.Models;
+using PlantsShop.Models.Entities;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PlantsShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private Context db;
+
+        public AccountController(Context context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+                if (user == null)
+                {
+                    user = new User { Email = model.Email, Password = model.Password };
+                    Role userRole = await db.Roles.FirstOrDefaultAsync(r => r.Name == "user");
+                    if (userRole != null)
+                        user.Role = userRole;
+
+                    db.Users.Add(user);
+                    await db.SaveChangesAsync();
+
+                    await Authenticate(user);
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users
+                    .Include(u => u.Role)
+                    .FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+                if (user != null)
+                {
+                    await Authenticate(user); // аутентификация
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private async Task Authenticate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role?.Name)
+            };
+
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PlantsShop.Models;
+using PlantsShop.Models.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PlantsShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        Context db;
+        IndexViewModel viewModel;
+        public HomeController(Context context)
+        {
+            db = context;
+            viewModel = new IndexViewModel();
+        }
+
+        public async Task<IActionResult> Index(int? seller, string name, bool isList, int page=1, SortState sortOrder = SortState.NameAsc)
+        {
+            IQueryable<Plant> plants = db.Plants;
+             
+            if (seller != null && seller != 0)
+            {
+                plants = plants.Where(p => p.SellerId == seller) ;
+            }
+            if (!String.IsNullOrEmpty(name))
+            {
+                plants = plants.Where(p => p.Name.Contains(name));
+            }
+
+            switch (sortOrder)
+            {
+                case SortState.NameDesc:
+                    plants = plants.OrderByDescending(s => s.Name);
+                    break;
+                case SortState.PriceAsc:
+                    plants = plants.OrderBy(s => s.Price);
+                    break;
+                case SortState.PriceDesc:
+                    plants = plants.OrderByDescending(s => s.Price);
+                    break;
+                case SortState.SellerAsc:
+                    plants = plants.OrderBy(s => s.Seller.Name);
+                    break;
+                case SortState.SellerDesc:
+                    plants = plants.OrderByDescending(s => s.Seller.Name);
+                    break;
+                default:
+                    plants = plants.OrderBy(s => s.Name);
+                    break;
+            }
+
+            int pageSize = 8;
+
+            var count = await plants.CountAsync();
+            var items = await plants.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            viewModel.PageViewModel = new PageViewModel(count, page, pageSize);
+            viewModel.SortViewModel = new SortViewModel(sortOrder);
+            viewModel.FilterViewModel = new FilterViewModel(db.Sellers.ToList(), seller, name);
+            viewModel.Plants = items;
+            viewModel.isList = isList;
+
+            return View(viewModel);
+        }
+    }
+}
