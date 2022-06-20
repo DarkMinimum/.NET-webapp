7b41f518aca36370abc42f1b6b25547df2689f4e
Danil <56466408+DarkMinimum@users.noreply.github.com>
2022-06-20 21:46:50 +0300

    Initial commit

---
+++ TagHelpers/ListGridTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace PlantsShop.TagHelpers
+{
+    public class ListGridTagHelper: TagHelper
+    {    
+        public bool isList { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            
+            if (isList == true)
+                output.Attributes.SetAttribute("class", "list left padding-top direction-column");
+            else
+                output.Attributes.SetAttribute("class", "list padding-top direction-row");
+                        
+        }
+    }
+}
---
+++ TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using PlantsShop.Models;
+
+namespace PlantsShop.TagHelpers
+{
+    public class PageLinkTagHelper: TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public IndexViewModel indexViewModel { get; set; } 
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            output.TagName = "nav";
+
+            // набор ссылок будет представлять список ul
+            TagBuilder tag = new TagBuilder("ul");
+            tag.AddCssClass("pagination");
+
+            // формируем три ссылки - на текущую, предыдущую и следующую
+            for (int i = 1; i <= indexViewModel.PageViewModel.TotalPages; i++)
+                tag.InnerHtml.AppendHtml(CreateTag(i, urlHelper));
+
+            output.Content.AppendHtml(tag);
+        }
+
+        TagBuilder CreateTag(int pageNumber, IUrlHelper urlHelper)
+        {
+            TagBuilder item = new TagBuilder("li");
+            TagBuilder link = new TagBuilder("a");
+            if (pageNumber == indexViewModel.PageViewModel.PageNumber)
+            {
+                item.AddCssClass("active");
+            }
+            else
+            {
+                link.Attributes["href"] = urlHelper.Action(PageAction, new { page = pageNumber, isList = indexViewModel.isList, name = indexViewModel.FilterViewModel.SelectedName, sortOrder = indexViewModel.SortViewModel.Current, seller = indexViewModel.FilterViewModel.SelectedSeller });
+            }
+            item.AddCssClass("page-item");
+            link.AddCssClass("page-link");
+            link.InnerHtml.Append(pageNumber.ToString());
+            item.InnerHtml.AppendHtml(link);
+            return item;
+        }
+    }
+}
